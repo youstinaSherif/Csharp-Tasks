06cbd46a0486003d0f031ec1d0f70822c5ba35aa
youstinaSherif <119603689+youstinaSherif@users.noreply.github.com>
2024-10-14 22:00:28 +0300

    Update Day1Task.cs

--- Day1Task.cs
+++ Day1Task.cs
@@ -11,41 +11,41 @@ public class Duration
         this.hours = hours;
         this.minutes = minutes;
         this.seconds = seconds;
-        NormalizeTime();
+        Normalize();
     }
 
-    public Duration(int totalSeconds)
+    public Duration(int totalSec)
     {
-        this.hours = totalSeconds / 3600;
-        this.minutes = (totalSeconds % 3600) / 60;
-        this.seconds = totalSeconds % 60;
+        this.hours = totalSec/3600;
+        this.minutes = (totalSec%3600) /60;
+        this.seconds = totalSec%60;
     }
 
-    private void NormalizeTime()
+    private void Normalize()
     {
         if (seconds >= 60)
         {
-            minutes += seconds / 60;
-            seconds %= 60;
+            minutes+= seconds/60;
+            seconds%= 60;
         }
 
         if (minutes >= 60)
         {
-            hours += minutes / 60;
-            minutes %= 60;
+            hours+= minutes/60;
+            minutes%= 60;
         }
     }
 
-    public string GetString()
+    public string getString()
     {
-        string result = "";
+        string result = " ";
 
         if (hours > 0)
         {
             result += $"Hours: {hours}, ";
         }
 
-        if (minutes > 0 || hours > 0)
+        if (minutes>0 || hours>0)
         {
             result += $"Minutes: {minutes}, ";
         }
@@ -60,15 +60,15 @@ public class Program
     public static void Main(string[] args)
     {
         Duration D1 = new Duration(1, 10, 15);
-        Console.WriteLine(D1.GetString());
+        Console.WriteLine(D1.getString());
         
         Duration D2 = new Duration(3600);
-        Console.WriteLine(D2.GetString());
+        Console.WriteLine(D2.getString());
         
         Duration D3 = new Duration(7800);
-        Console.WriteLine(D3.GetString());
+        Console.WriteLine(D3.getString());
         
         Duration D4 = new Duration(666);
-        Console.WriteLine(D4.GetString());
+        Console.WriteLine(D4.getString());
     }
 }
